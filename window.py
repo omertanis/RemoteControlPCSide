6c7bac1c64b9a57433ed9943d687bbc6abda7d63
omertanis <omer.tanis@gazi.edu.tr>
2018-04-05 20:52:47 +0300

    foregroundlar değişti e icon eklendi.

--- window.py
+++ window.py
@@ -44,35 +44,35 @@ class Application():
         # ----------
         # logo
         import PIL.ImageTk
-        space1 = Label(frameMain, text="\n\n", background='#299FD2')
+        space1 = Label(frameMain, text="\n\n", background='#299FD2',foreground="#12455A")
         space1.pack()
 
         im = PIL.Image.open("logo.png")
         im = im.resize((150, 150))
         photo = PIL.ImageTk.PhotoImage(im)
-        label = Label(frameMain, image=photo, background='#299FD2')
+        label = Label(frameMain, image=photo, background='#299FD2',foreground="#12455A")
         label.image = photo
         label.pack()
 
         # -----------
-        labelMain = Label(frameMain, textvariable=labelVar, background='#299FD2')
+        labelMain = Label(frameMain, textvariable=labelVar, background='#299FD2',foreground="#12455A")
         labelMain.config(font=("Helvetica", 40))
         labelVar.set("Remote Control")
         labelMain.pack()
 
-        R1 = Radiobutton(frameMain, text="Bluetooth", variable=var, value=0, background='#299FD2')
+        R1 = Radiobutton(frameMain, text="Bluetooth", variable=var, value=0, background='#299FD2',foreground="#12455A")
         R1.config(font=("Helvetica", 20))
 
         R1.pack()
 
-        R2 = Radiobutton(frameMain, text="Wifi", variable=var, value=1, background='#299FD2')
+        R2 = Radiobutton(frameMain, text="Wifi", variable=var, value=1, background='#299FD2',foreground="#12455A")
         R2.config(font=("Helvetica", 20))
         R2.pack()
 
-        space2 = Label(frameMain, text="\n", background='#299FD2')
+        space2 = Label(frameMain, text="\n", background='#299FD2',foreground="#12455A")
         space2.pack()
 
-        B = Button(frameMain, text="Cihaz Ara", command=self.helloCallBack, background='#b3c6e5')
+        B = Button(frameMain, text="Cihaz Ara", command=self.helloCallBack, background='#b3c6e5',foreground="#12455A")
         B.config(font=("Helvetica", 20))
         B.pack()
         self.createWifiQr()
@@ -84,7 +84,14 @@ class Application():
         frameBluetooth.configure(background='#299FD2')
 
         space3 = Label(frameBluetooth, text="\n\n", background='#299FD2')
-        space3.pack()
+        # space3.pack()
+        labelBluetoothVar = StringVar()
+
+        labelBluetooth = Label(frameBluetooth, textvariable=labelBluetoothVar, background='#299FD2', foreground="#12455A")
+        labelBluetooth.config(font=("Helvetica", 40))
+        labelBluetoothVar.set("Bluetooth")
+        labelBluetooth.pack()
+
         im = PIL.Image.open("bluetooth.png")
         im = im.resize((300, 300))
         photo = PIL.ImageTk.PhotoImage(im)
@@ -92,9 +99,9 @@ class Application():
         label = Label(frameBluetooth, image=photo, background='#299FD2')
         label.image = photo
         label.pack()
-        space4 = Label(frameBluetooth, text="\n\n", background='#299FD2')
+        space4 = Label(frameBluetooth, text="\n", background='#299FD2')
         space4.pack()
-        B = Button(frameBluetooth, text="Geri", command=self.backOnClick, background='#b3c6e5')
+        B = Button(frameBluetooth, text="Geri", command=self.backOnClick, background='#b3c6e5',foreground="#12455A")
         B.config(font=("Helvetica", 20))
         B.pack()
 
@@ -106,19 +113,25 @@ class Application():
         frameWifi.configure(background='#299FD2')
 
         space5 = Label(frameWifi, text="\n\n", background='#299FD2')
-        space5.pack()
+        # space5.pack()
+        labelWifiVar = StringVar()
+        labelWifi = Label(frameWifi, textvariable=labelWifiVar, background='#299FD2', foreground="#12455A")
+        labelWifi.config(font=("Helvetica", 40))
+        labelWifiVar.set("Wi-Fi")
+        labelWifi.pack()
+
 
         im = PIL.Image.open("wifi.png")
         im = im.resize((300, 300))
         photo = PIL.ImageTk.PhotoImage(im)
-        label = Label(frameWifi, image=photo, background='#299FD2')
+        label = Label(frameWifi, image=photo, background='#299FD2', foreground="#12455A")
         label.image = photo
         label.pack()
 
-        space6 = Label(frameWifi, text="\n\n", background='#299FD2')
+        space6 = Label(frameWifi, text="\n", background='#299FD2')
         space6.pack()
 
-        B = Button(frameWifi, text="Geri", command=self.backOnClick, background='#b3c6e5')
+        B = Button(frameWifi, text="Geri", command=self.backOnClick, background='#b3c6e5',foreground="#12455A")
         B.config(font=("Helvetica", 20))
         B.pack()
 
@@ -146,9 +159,10 @@ if __name__ == '__main__':
     root.minsize(width=500, height=500)
     root.resizable(width=False, height=False)
     root.configure(background='#299FD2')
-    root.title(' TK Wizards ')
+    root.title(' Remote Control ')
     var = IntVar()
     frameMain = Frame(root)
     frameBluetooth = Frame(root)
     frameWifi = Frame(root)
+    root.iconbitmap('logo.ico')
     app = Application(master=root)
